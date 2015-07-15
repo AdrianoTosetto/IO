6f8d3c0b0f5249e70d5b54e2490a7a41a954523d
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-07-15 19:39:51 -0300

    Update File.c

--- File.c
+++ File.c
@@ -9,7 +9,7 @@
 extern int errno;
 
 string readFile(const char *filename){
-	FILE *file = fopen(filename,"r+");
+    FILE *file = fopen(filename,"r+");
 	
     string buffer = NULL;
     
@@ -22,9 +22,9 @@ string readFile(const char *filename){
         if (n + 1 > capacity){
             if (capacity == 0){
                 capacity = 32;
-			}else if (capacity <= (UINT_MAX / 2)){
+	}else if (capacity <= (UINT_MAX / 2)){
                 capacity *= 2;
-			}
+	}
             else{
                 free(buffer);
                 return NULL;
@@ -45,10 +45,10 @@ string readFile(const char *filename){
 
     if (n == 0 && c == EOF){
     	fclose(file);
- 	   	int err = errno;
-   	    fprintf(stderr, "Error: %s\n", strerror(err));
+ 	int err = errno;
+   	fprintf(stderr, "Error: %s\n", strerror(err));
         return NULL;
-	}
+    }
 
     string minimal = (char *)malloc((n + 1) * sizeof(char));
     strncpy(minimal, buffer, n);
@@ -69,7 +69,7 @@ void writeFile(const string fileName,bool overwrite,const char *format,...){
 		file = fopen(fileName,"a+");
 	}
 	if(file == NULL){
-		int err = errno;
+	    int err = errno;
    	    fprintf(stderr, "Error: %s\n", strerror(err));
    	    return ;
 	}
