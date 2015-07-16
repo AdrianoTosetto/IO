a3480cbb5cb73f0bc2cdf06bce504fb9282aec54
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-07-16 00:26:21 -0300

    Update File.h

--- File.h
+++ File.h
@@ -1,15 +1,16 @@
 #ifndef _FILE_H
 #define _FILE_H
 #endif
-
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef char* string;
+typedef unsigned int uint;
 
 
 /**
  * reads a file and returs it's content
  */
-
 string readFile(const char* filename);
 
 void writeFile(const string fileName,bool overwrite,const char *format,...);
@@ -17,3 +18,5 @@ void writeFile(const string fileName,bool overwrite,const char *format,...);
 //clear buffer
 
 void clsBuffer();
+
+string getString();
