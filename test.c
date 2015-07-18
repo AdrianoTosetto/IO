52c140d8f6ae08b4fc7c154e1408ac9354d76aef
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-07-18 19:43:50 -0300

    Update test.c

--- test.c
+++ test.c
@@ -2,11 +2,26 @@
 #include "File.h"
 
 int main(int argc, char** argv){
-  
+  //example for readFile
   const char *filename = "myfile.txt";
   string txt = readFile(filename);
   printf(txt); //prints the content of myfile.txt
   
+  
+  //example for tokenizer
+  char **tokens = tokenizer("my string I want to divide"," ");
+	int i;
+	for(i = 0; i < 6;i++){
+		printf("%s\n",tokens[i]);
+	}
+		/**
+	* it outputs : my
+	* 			   string
+	* 			   I
+	* 			   want
+	* 			   to 
+	* 			   divide
+ 	*/
   
   return 0;
 }
