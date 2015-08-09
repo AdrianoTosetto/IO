3574d1840260a92fd472f3689413014734abc146
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-08-08 23:12:32 -0300

    Update test.c

--- test.c
+++ test.c
@@ -9,22 +9,35 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 	
-	char **tokens = tokenizer(readFile("teste.txt")," ");
-	int i;
-	for(i = 0; tokens[i] != NULL;i++){
-		printf("%s\n",tokens[i]);
+	//Test for getString and readFile
+	printf("Put the name of the file: \n");
+	string fileName = getString();     //reads an input from the keyboard(stdin)
+	printf(readFile(fileName)); //gets the content of fileName
+	
+	
+	//Test for tokenizer
+	printf("Put a string to split: \n");
+	string str = getString();
+	printf("Put the delimiter: \n");
+	string delimiter = getString();
+	char **tokens = tokenizer(str,delimiter);
+	
+	for(int i = 0; tokens[i] != NULL;i++){
+		printf("Tokens number %d: %s\n",i+1,tokens[i]);
 	}
 	
-	/**
-	* tokens[i] == NULL tells where the tokens array finishes 
-	* it outputs : my
-	* 			   string
-	* 			   I
-	* 			   want
-	* 			   to 
-	* 			   divide
- 	*/
+	//test for match
 	
+	
+	printf("Put the string: \n");
+		string str = getString();
+		printf("Put the substring: \n");
+		string substr = getString();
+		if(match(str,substr)){
+			printf("Substring: %s exists in main string: %s\n",substr,str);
+		}else{
+			printf("Substring: %s does not exists in main string: %s\n",substr,str);
+		}
 	
     return 0;       
 }
